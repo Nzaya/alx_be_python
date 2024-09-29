7ff7edbca82c0a8023ab6e36660c59001ec91e20
Sharon Nzaya <45424644+Nzaya@users.noreply.github.com>
2024-09-29 20:15:37 +0300

    Update arithmetic divide by zero

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -20,9 +20,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
-            return "Cannot divide by zero."  # Handle division by zero.
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."  # Return a consistent message for division by zero.
+        return num1 / num2  # Perform division if num2 is not zero.
     else:
         return "Invalid operation"  # Return a message if the operation is not recognized.
